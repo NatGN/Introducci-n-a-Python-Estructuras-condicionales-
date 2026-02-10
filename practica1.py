e0d48c5bc21bd23bd8edfb485a93ea7ad891aba7
NatGN <nataliago845@gmail.com>
2026-02-10 08:56:35 -0600

    Add files via upload

---
+++ practica1.py
@@ -0,0 +1,56 @@
+#1
+numero=4
+if numero %2 ==0:
+    print("Par")
+else:
+    print("Impar")
+    
+#2
+n=6
+if numero>0<numero:
+    print("Positivo")
+else:
+    print("Negativo")
+    
+#3
+edad=45
+if edad>=18:
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
+
+#4
+calif=80
+if calif>=60:
+    print("Aprobado")
+else:
+    print("Reprobado")
+
+#5
+calificacion=40
+if calificacion >= 90:
+    print("A")
+elif calificacion >= 80:
+    print("B")
+elif calificacion >= 70:
+    print("C")
+elif calificacion >= 60:
+    print("D")
+else:
+    print("F")
+
+#6
+temperatura = 25
+
+if temperatura < 0:
+    print("Solido")
+elif temperatura <= 100:
+    print("Liquido")
+else:
+    print("Vapor")
+
+
+    
+
+
+
